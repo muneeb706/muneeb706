a842e3ae920e87c6957e303893eb5230e6948764
Muneeb Shahid <muneeb706@gmail.com>
2024-05-26 19:03:26 -0700

    feat(build_readme): Update build_readme.py to add VSCode extensions in README.md

--- build_readme.py
+++ build_readme.py
@@ -17,19 +17,17 @@ def filter_vscode_extensions(repos):
     return vscode_extensions
 
 def add_vscode_ext_repos(repos):
-    with open('README.md', 'r+') as f:
-        contents = f.read()
-        try:
-            index = contents.index('### Visual Studio Code Extensions\n')
-            print(f'Text already exists at index {index}')
-        except ValueError:
-            f.write('### Visual Studio Code Extensions\n')
-    # with open('README.md', 'w') as f:
-    #     f.write('### Visual Studio Code Extensions\n')
-    #     for repo in repos:
-    #         f.write(f'- [{repo["name"]}]({repo["homepage"]})\n')
-                    
+    content = '### Visual Studio Code Extensions\n'
+    for repo in repos:
+        content += f'- [{repo["name"]}]({repo["homepage"]})\n'
+    return content
+
+def update_readme(content):
+    with open('README.md', 'w') as file:
+        file.writelines(content)
+
 if __name__ == "__main__":
     repos = fetch_all_repos()
     vscode_ext_repos = filter_vscode_extensions(repos)
-    add_vscode_ext_repos(vscode_ext_repos)
+    readme_content = add_vscode_ext_repos(vscode_ext_repos)
+    update_readme(readme_content)
