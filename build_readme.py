9472ac4fe9310fe187e02c3c770553d20df23a56
Muneeb Shahid <muneeb706@gmail.com>
2024-05-26 19:09:21 -0700

    feat(build_readme): Add technologies table to README.md

--- build_readme.py
+++ build_readme.py
@@ -22,12 +22,23 @@ def add_vscode_ext_repos(repos):
         content += f'- [{repo["name"]}]({repo["homepage"]})\n'
     return content
 
+def add_technologies():
+    content = '\n'
+    content += '### Technologies\n\n'
+    content += '| Backend  | Frontend | Database | Cloud & DevOps | Testing |\n'
+    content += '| ------------- | ------------- | ------------- | ------------- | ------------- |\n'
+    content += '|[![Backend](https://skillicons.dev/icons?i=py,django,java,spring&theme=light)](https://skillicons.dev)|[![Frontend](https://skillicons.dev/icons?i=js,ts,react,vite&theme=light)](https://skillicons.dev)|[![Database](https://skillicons.dev/icons?i=postgres,mongodb&theme=light)](https://skillicons.dev)|[![Cloud&DevOps](https://skillicons.dev/icons?i=aws,docker&theme=light)](https://skillicons.dev)|[![Testing](https://skillicons.dev/icons?i=selenium,cypress&theme=light)](https://skillicons.dev)|\n'
+
+    return content
+
 def update_readme(content):
     with open('README.md', 'w') as file:
         file.writelines(content)
+
 
 if __name__ == "__main__":
     repos = fetch_all_repos()
     vscode_ext_repos = filter_vscode_extensions(repos)
     readme_content = add_vscode_ext_repos(vscode_ext_repos)
+    readme_content += add_technologies()
     update_readme(readme_content)
