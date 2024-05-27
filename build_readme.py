9cc100525bc4fc1192dba66cffc9a4450e2d22af
Muneeb Shahid <muneeb706@gmail.com>
2024-05-26 17:29:07 -0700

    feat(build_readme): Add function to fetch and filter VSCode extensions

--- build_readme.py
+++ build_readme.py
@@ -1,6 +1,23 @@
 import os
+import requests
 
 TOKEN = os.environ.get("MUNEEB706_TOKEN", "")
 
+def fetch_all_repos():
+    headers = {'Authorization': f'token {TOKEN}'}
+    response = requests.get('https://api.github.com/users/muneeb706/repos', headers=headers)
+    repos = []
+    if response.status_code == 200:
+        repos = response.json()
+
+    return repos
+
+def filter_vscode_extensions(repos):
+    vscode_extensions = [repo for repo in repos if 'vscode-extension' in repo.get('topics', [])]
+    return vscode_extensions
+
+
 if __name__ == "__main__":
-    print("Hello World", True if TOKEN else False)
+    repos = fetch_all_repos()
+    vscode_ext_repos = filter_vscode_extensions(repos)
+    print(vscode_ext_repos)
